688c6e117112481e9da5409ac6955e8871a6ea93
Roberto Polli <robipolli@gmail.com>
2023-11-14 20:47:59 +0100

    Fix: #20. Refactoring test folders.

--- tests/test_semgrep.py
+++ tests/test_semgrep.py
@@ -1,37 +1,23 @@
 from pathlib import Path
 
+import pytest
+
 import parse_scripts.semgrep
 from parse_scripts.util import json_load
 
-DATA_DIR = Path(__file__).parent / "json"
-
-expected_results = {
-    "name": "Semgrep Comments",
-    "head_sha": "stuff",
-    "completed_at": "2023-11-09T15:29:33.821590Z",
-    "conclusion": "failure",
-    "output": {
-        "title": "Semgrep: ",
-        "summary": 'Semgrep statistics: {\n  "Total_errors": 1,\n  "Semgrep_Version": "1.34.0",\n  "paths_scanned": 36\n}',
-        "text": "<add --verbose for a list of skipped paths>",
-        "annotations": [
-            {
-                "path": ".github/workflows/test.yml",
-                "start_line": 31,
-                "end_line": 31,
-                "start_column": 114,
-                "end_column": 117,
-                "annotation_level": "warning",
-                "title": "Syntax error",
-                "message": " When parsing a snippet as Bash for metavariable-pattern in rule 'yaml.github-actions.security.curl-eval.curl-eval'",
-            }
-        ],
-    },
-}
+DATA_DIR = Path(__file__).parent / "data"
 
 
-def test_parse_data():
-    data = json_load(DATA_DIR / "semgrep.json")
+@pytest.mark.parametrize(
+    "infile,expected",
+    [
+        (f, f.with_suffix(".annotations.json"))
+        for f in DATA_DIR.glob("semgrep-[0-9][0-9].json")
+    ],
+)
+def test_parse_data(infile, expected):
+    data = json_load(infile)
+    expected_results = json_load(expected)
     actual_results = parse_scripts.semgrep.parse_data(data, "stuff")
     actual_results["completed_at"] = "2023-11-09T15:29:33.821590Z"
     assert expected_results == actual_results
